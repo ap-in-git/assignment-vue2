fde2133cc076daf66da8e03d2ebc3f1c33ae8aae
SyKim1201 <b_ri_k@hotmail.com>
2022-02-07 09:48:26 -0500

    Fixed some bugs in cart and review

--- src/components/Toolbar.vue
+++ src/components/Toolbar.vue
@@ -1,7 +1,12 @@
 <template>
   <div>
     <v-app-bar elevation="0">
-      <a href="#" @click="$router.push({name:'Index'})" style="text-decoration: none;color: black;font-size: 2rem;">Cofye</a>
+      <a
+        href="#"
+        @click="$router.push({ name: 'Index' })"
+        style="text-decoration: none; color: black; font-size: 2rem"
+        >Cofye</a
+      >
       <v-spacer></v-spacer>
       <span v-if="isLoggedIn">
         {{ loggedInUserDetail }}
@@ -12,28 +17,19 @@
       <v-btn :icon="true" @click="drawer = !drawer">
         <v-icon>mdi-cart-outline</v-icon>
       </v-btn>
-      <v-btn :icon="true"  v-if="isLoggedIn" @click="logoutUser">
+      <v-btn :icon="true" v-if="isLoggedIn" @click="logoutUser">
         <v-icon>mdi-logout</v-icon>
       </v-btn>
     </v-app-bar>
-    <v-dialog
-        v-model="dialog"
-       width="500"
-    >
+    <v-dialog v-model="dialog" width="500">
       <v-card style="width: 700px">
-        <v-card-title class="text-h5 grey lighten-2">
-          Login
-        </v-card-title>
-        <v-form
-            ref="form"
-            v-model="valid"
-            lazy-validation
-        >
-        <v-card-text>
-          <v-alert type="error" v-if="errorMessage !== ''" class="mb-4">
-            {{errorMessage }}
-          </v-alert>
-          <v-text-field
+        <v-card-title class="text-h5 grey lighten-2"> Login </v-card-title>
+        <v-form ref="form" v-model="valid" lazy-validation>
+          <v-card-text>
+            <v-alert type="error" v-if="errorMessage !== ''" class="mb-4">
+              {{ errorMessage }}
+            </v-alert>
+            <v-text-field
               variant="outlined"
               prepend-inner-icon="mdi-at"
               name="email"
@@ -42,8 +38,8 @@
               :rules="nameRules"
               v-model="email"
               required
-          ></v-text-field>
-          <v-text-field
+            ></v-text-field>
+            <v-text-field
               id="password"
               variant="outlined"
               prepend-inner-icon="mdi-key"
@@ -53,69 +49,81 @@
               v-model="password"
               type="password"
               required
-          ></v-text-field>
-          <v-btn color="primary" class="mt-4" block @click="login"> Login </v-btn>
-          <v-btn variant="outlined" @click.prevent="dialog = false" block="" class="mt-5">
-            Close
-          </v-btn>
-        </v-card-text>
+            ></v-text-field>
+            <v-btn color="primary" class="mt-4" block @click="login">
+              Login
+            </v-btn>
+            <v-btn
+              variant="outlined"
+              @click.prevent="dialog = false"
+              block=""
+              class="mt-5"
+            >
+              Close
+            </v-btn>
+          </v-card-text>
         </v-form>
       </v-card>
     </v-dialog>
     <v-navigation-drawer
-        :width="360"
-        v-model="drawer"
-        temporary
-        absolute
-        right
-        class="px-2"
+      :width="360"
+      v-model="drawer"
+      temporary
+      absolute
+      right
+      class="px-2"
     >
-      <div class="d-flex flex-column justify-space-between " style="height: 100%;">
+      <div
+        class="d-flex flex-column justify-space-between"
+        style="height: 100%"
+      >
         <div>
-          <div class="py-6 font-weight-bold display-1">
-            Shopping cart
-          </div>
-          <v-divider/>
+          <div class="py-6 font-weight-bold display-1">Shopping cart</div>
+          <v-divider />
           <div v-for="cartItem in cartItems" :key="cartItem.slug" class="mt-2">
             <div class="d-flex">
-              <v-img :width="75" :height="75" :src="cartItem.image"/>
+              <v-img :width="75" :height="75" :src="cartItem.image" />
               <div class="ml-1">
                 <div class="d-flex justify-space-between">
                   <div class="font-weight-bold">
-                    {{cartItem.name}}
+                    {{ cartItem.name }}
                   </div>
-                  <v-icon size="small" @click="removeItem(cartItem)">mdi-close</v-icon>
+                  <v-icon size="small" @click="removeItem(cartItem)"
+                    >mdi-close</v-icon
+                  >
                 </div>
                 <div class="d-flex mt-3 justify-space-between">
+                  <div style="max-width: 60%">
                     <v-text-field
-                        class="pr-15"
-                        solo
-                        hide-details="auto"
-                        dense
-                        prepend-inner-icon="mdi-minus"
-                        append-icon="mdi-plus"
-                        :value="cartItem.quantity"
-                        @click:prepend-inner="decrement(cartItem)"
-                        @click:append="increment(cartItem)"
+                      class="pr-15"
+                      solo
+                      hide-details="auto"
+                      dense
+                      readonly
+                      prepend-inner-icon="mdi-minus"
+                      append-icon="mdi-plus"
+                      :value="cartItem.quantity"
+                      @click:prepend-inner="decrement(cartItem)"
+                      @click:append="increment(cartItem)"
                     >
                     </v-text-field>
-                  <div>
-                    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;$&nbsp;&nbsp;{{  cartItem.quantity * cartItem.price }}
+                  </div>
+                  <div class="d-flex justify-end">
+                    $&nbsp;&nbsp;{{ cartItem.quantity * cartItem.price }}
                   </div>
                 </div>
-
               </div>
             </div>
           </div>
           <div v-if="cartItems.length === 0">
-            <div class="py-6 font-weight-bold ">
-              No items in shopping cart
-            </div>
+            <div class="py-6 font-weight-bold">No items in shopping cart</div>
           </div>
-
         </div>
         <div class="pb-8">
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci alias amet blanditiis doloribus eos est et exercitationem fuga ipsum, molestias neque, officiis repellat tempore tenetur, vero vitae voluptates voluptatum. Esse.
+          <!--           Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci
+          alias amet blanditiis doloribus eos est et exercitationem fuga ipsum,
+          molestias neque, officiis repellat tempore tenetur, vero vitae
+          voluptates voluptatum. Esse. -->
           <div v-if="cartItems.length > 0">
             <v-btn variant="outlined" color="primary" block="">
               Proceed to checkout
@@ -123,87 +131,78 @@
           </div>
         </div>
       </div>
-
-
-
     </v-navigation-drawer>
   </div>
 </template>
 
 <script>
 export default {
-  data(){
+  data() {
     return {
       valid: true,
-      dialog:false,
-      errorMessage :"",
-      nameRules: [
-        v => !!v || 'Name is required',
-      ],
-      passwordRules: [
-        v => !!v || 'Password is required',
-      ],
-      email:"",
-      password:""
-    }
+      dialog: false,
+      errorMessage: "",
+      nameRules: [(v) => !!v || "Name is required"],
+      passwordRules: [(v) => !!v || "Password is required"],
+      email: "",
+      password: "",
+    };
   },
   name: "Toolbar",
-  methods:{
-    handleClose(val){
-      console.log(val)
+  methods: {
+    handleClose(val) {
+      console.log(val);
     },
-    logoutUser(){
-      this.$store.commit("user/setLoggedIn",false)
+    logoutUser() {
+      this.$store.commit("user/setLoggedIn", false);
     },
-    login(){
-      this.errorMessage = ''
+    login() {
+      this.errorMessage = "";
       if (this.email === "") {
-        this.errorMessage ="Email is required"
-        return
+        this.errorMessage = "Email is required";
+        return;
       }
       if (this.password === "") {
-        this.errorMessage = "Password is required"
+        this.errorMessage = "Password is required";
       }
       if (this.email === "hi@test.com" && this.password === "password") {
-        this.$store.commit("user/setLoggedIn",true)
-        this.dialog = false
-      }else{
-        this.errorMessage = "Email or password is incorrect"
+        this.$store.commit("user/setLoggedIn", true);
+        this.dialog = false;
+      } else {
+        this.errorMessage = "Email or password is incorrect";
       }
     },
-    increment(product){
-      this.$store.commit("cart/increment",product)
+    increment(product) {
+      this.$store.commit("cart/increment", product);
     },
-    decrement(product){
-      this.$store.commit("cart/decrement",product)
+    decrement(product) {
+      this.$store.commit("cart/decrement", product);
     },
-    removeItem(product){
-      this.$store.commit("cart/removeItem",product)
-    }
+    removeItem(product) {
+      this.$store.commit("cart/removeItem", product);
+    },
   },
-  computed:{
-    loggedInUserDetail(){
+  computed: {
+    loggedInUserDetail() {
       return this.$store.state.user.userDetail.name;
     },
-    isLoggedIn(){
+    isLoggedIn() {
       return this.$store.state.user.isLoggedIn;
     },
 
-    cartItems(){
+    cartItems() {
       return this.$store.state.cart.items;
     },
-    drawer:{
-      get(){
-        return this.$store.state.cart.drawer
+    drawer: {
+      get() {
+        return this.$store.state.cart.drawer;
       },
-      set(val){
-        this.$store.commit("cart/toggleDrawer",val)
-      }
+      set(val) {
+        this.$store.commit("cart/toggleDrawer", val);
+      },
     },
-  }
-}
+  },
+};
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
--- src/pages/Product.vue
+++ src/pages/Product.vue
@@ -1,53 +1,65 @@
 <template>
   <div>
-    <v-container fluid style="padding:0">
+    <v-container fluid style="padding: 0">
       <v-row>
-      <v-col sm="7" style="padding-bottom: 0">
-        <v-img :src="product.bannerImage"/>
-      </v-col>
-        <v-col sm="5" class="pl-16 pt-10 pr-16" style="background-color: #F7F3EE;">
+        <v-col sm="7" style="padding-bottom: 0">
+          <v-img :src="product.bannerImage" />
+        </v-col>
+        <v-col
+          sm="5"
+          class="pl-16 pt-10 pr-16"
+          style="background-color: #f7f3ee"
+        >
           <p class="text-h4 pt-12 pb-6">{{ product.name }}</p>
-          <p class="text-h5">${{product.price}}</p>
-          <div class="d-flex  align-center">
+          <p class="text-h5">${{ product.price }}</p>
+          <div class="d-flex align-center">
             <v-rating
-                v-model="averageReview"
-                :readonly="true"
-                size="14"
+              v-model="averageReview"
+              :readonly="true"
+              size="14"
             ></v-rating>
             <span class="grey--text text--lighten-2 text-caption ml-2">
-                ({{ reviewCount }} reviews)
+              ({{ reviewCount }} reviews)
             </span>
           </div>
           <p>
-            The {{ product.name }} coffee maker combines precision brewing, top-quality construction, and a design that will elevate any environment.
-            Designed for perfect brewing from 16 to 40 ounces, the Eight uses a smart brewing algorithm to mimic hand-made pour over coffee.
+            The {{ product.name }} coffee maker combines precision brewing,
+            top-quality construction, and a design that will elevate any
+            environment. Designed for perfect brewing from 16 to 40 ounces, the
+            Eight uses a smart brewing algorithm to mimic hand-made pour over
+            coffee.
           </p>
           <v-row class="d-flex justify-center">
-            <div style="width: 320px;">
-              <v-btn class="mt-10" outlined :block="true"               @click="addProductToCart(product)">Add to cart</v-btn>
+            <div style="width: 320px">
+              <v-btn
+                class="mt-10"
+                outlined
+                :block="true"
+                @click="addProductToCart(product)"
+                >Add to cart</v-btn
+              >
             </div>
           </v-row>
         </v-col>
       </v-row>
     </v-container>
-    <v-container  class="mt-10">
+    <v-container class="mt-10">
       <v-row class="my-6">
-        <p class="text-h4" >Reviews</p>
-        <v-col sm="12"             v-for="item in reviews"             :key="item._id">
-          <v-card
-              class="my-2"
-              elevation="0"
-          >
-            <v-card-title >
-              <span class="font-weight-bold mr-4">{{item.commentedBy}}</span>
-              <small class="grey--text">{{new Date(item.commentedAt).toLocaleString()}}</small>
+        <p class="text-h4">Reviews</p>
+        <v-col sm="12" v-for="item in reviews" :key="item._id">
+          <v-card v-if="product.slug === item.slug" class="my-2" elevation="0">
+            <v-card-title>
+              <span class="font-weight-bold mr-4">{{ item.commentedBy }}</span>
+              <small class="grey--text">{{
+                new Date(item.commentedAt).toLocaleString()
+              }}</small>
               <v-spacer />
               <v-rating
-                  :readonly="true"
-                  :model-value="parseInt(item.rating)"
-                  :length="5"
-                  small
-                  color="secondary"
+                :readonly="true"
+                :value="parseInt(item.rating)"
+                :length="5"
+                small
+                color="secondary"
               ></v-rating>
             </v-card-title>
             <v-card-text>
@@ -55,51 +67,48 @@
             </v-card-text>
           </v-card>
         </v-col>
-
       </v-row>
       <h3 class="mt-5">Review the product</h3>
       <v-divider />
       <v-form @submit.prevent="submitForm">
         <v-rating
-            v-model="rating"
-            background-color="secondary"
-            color="secondary"
+          v-model="rating"
+          background-color="secondary"
+          color="secondary"
         ></v-rating>
         <v-text-field
-            name="input-7-1"
-            class="mt-1"
-            outlined
-            label="Tell us your feedback about the product"
-            v-model="commentedText"
+          name="input-7-1"
+          class="mt-1"
+          outlined
+          label="Tell us your feedback about the product"
+          v-model="commentedText"
         ></v-text-field>
         <div class="mt-1 text-right">
           <v-btn color="secondary" type="submit">Submit</v-btn>
         </div>
       </v-form>
     </v-container>
-    <v-container  class="mt-10">
+    <v-container class="mt-10">
       <v-row class="my-6">
         <p class="text-h4" style="color: #795548">Related Products</p>
       </v-row>
-      <div class="d-flex justify-space-between align-center" >
-        <div  v-for="relatedProduct in relatedProducts" :key="relatedProduct.slug" class="d-flex align-center  justify-center" style="flex-direction: column;cursor: pointer" @click="goToProduct(relatedProduct.slug)">
-          <v-img
-              height="200"
-              width="200"
-              :src="relatedProduct.image"
-          ></v-img>
-          <p style="font-size: 2rem;">
-            {{relatedProduct.name}}
+      <div class="d-flex justify-space-between align-center">
+        <div
+          v-for="relatedProduct in relatedProducts"
+          :key="relatedProduct.slug"
+          class="d-flex align-center justify-center"
+          style="flex-direction: column; cursor: pointer"
+          @click="goToProduct(relatedProduct.slug)"
+        >
+          <v-img height="200" width="200" :src="relatedProduct.image"></v-img>
+          <p style="font-size: 2rem">
+            {{ relatedProduct.name }}
           </p>
-          <p style="font-size: 24px;">
-            $ {{product.price}}
-          </p>
+          <p style="font-size: 24px">$ {{ relatedProduct.price }}</p>
         </div>
       </div>
-
     </v-container>
   </div>
-
 </template>
 
 <script>
@@ -107,64 +116,76 @@ export default {
   name: "Product",
   data: () => ({
     rating: 5,
-    commentedText:"",
+    commentedText: "",
   }),
   computed: {
-    averageReview(){
-      const reviews = this.$store.state.product.reviews;
+    averageReview() {
+      const slug = this.$route.params.slug;
+      const reviews = this.$store.state.product.reviews.filter(
+        (p) => p.slug === slug
+      );
       if (reviews.length === 0) {
         return 0;
       }
       let totalReview = 0;
 
-      for (let i = 0; i < reviews.length ; i++) {
+      for (let i = 0; i < reviews.length; i++) {
         totalReview = totalReview + reviews[i].rating;
       }
-      return  totalReview/reviews.length;
+      return totalReview / reviews.length;
     },
-    reviews(){
-      return this.$store.state.product.reviews;
+    reviews() {
+      const slug = this.$route.params.slug;
+      return this.$store.state.product.reviews.filter((p) => p.slug === slug);
     },
-    reviewCount(){
-      return this.$store.state.product.reviews.length;
+    reviewCount() {
+      const slug = this.$route.params.slug;
+      return this.$store.state.product.reviews.filter((p) => p.slug === slug)
+        .length;
     },
-    products(){
-       return this.$store.state.product.products;
-   } ,
+    products() {
+      return this.$store.state.product.products;
+    },
     product() {
       const slug = this.$route.params.slug;
-      return  this.products.find(p => p.slug === slug);
+      return this.products.find((p) => p.slug === slug);
     },
     relatedProducts() {
       const slug = this.$route.params.slug;
-      return this.products.filter( p => p.slug !== slug)
-    }
+      return this.products.filter((p) => p.slug !== slug);
+    },
   },
   methods: {
     addProductToCart(product) {
-      this.$store.commit("cart/addProductToCart",product)
-      this.$store.commit("cart/toggleDrawer",true)
+      this.$store.commit("cart/addProductToCart", product);
+      this.$store.commit("cart/toggleDrawer", true);
     },
-    submitForm(){
-      if (this.commentedText === ""){
-        this.$store.dispatch("notification/showErrorMessage","Review text is missing")
-        return
+    submitForm() {
+      if (this.commentedText === "") {
+        this.$store.dispatch(
+          "notification/showErrorMessage",
+          "Review text is missing"
+        );
+        return;
       }
-      this.$store.commit("product/addReview",{
-        _id: this.reviewCount+1,
-        commentedAt: new Date().toDateString(),
-        rating:this.rating,
-        text:this.commentedText,
-        commentedBy:"Ashok Poudel",
-        slug:this.$route.params.slug,
-      })
-      this.commentedText = ""
-      this.rating = 5
-      this.$store.dispatch("notification/showSuccessMessage","Review added successfully")
+      this.$store.commit("product/addReview", {
+        _id: this.reviewCount + 1,
+        commentedAt: new Date().toLocaleString(),
+        rating: this.rating,
+        text: this.commentedText,
+        commentedBy: "Ashok Poudel",
+        slug: this.$route.params.slug,
+      });
+      this.commentedText = "";
+      this.rating = 5;
+      this.$store.dispatch(
+        "notification/showSuccessMessage",
+        "Review added successfully"
+      );
     },
-    goToProduct(slug){
-      this.$router.push({name: "Product",params:{slug}})
-    }
+    goToProduct(slug) {
+      this.$router.push({ name: "Product", params: { slug } });
+    },
   },
 };
 </script>
