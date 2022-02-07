929ffa709a2f53c087fb49dd2bd7c40fbf79bbb4
KannuSingh <karandeepsinghcaj@outlook.com>
2022-02-07 12:51:35 -0500

    Changed ProductList component
    Made product card clickable in ProductList component

--- src/components/ProductList.vue
+++ src/components/ProductList.vue
@@ -2,56 +2,57 @@
   <v-container>
     <div class="display-1 font-weight-bold">Products</div>
     <v-row wrap>
-    <v-col cols="3" v-for="product in products" :key="product.slug">
-      <v-card elevation="1" class="my-4 py-4">
-        <v-card-text class="text-center">
-          <v-img
+      <v-col cols="3" v-for="product in products" :key="product.slug">
+        <v-card
+          elevation="1"
+          class="my-4 py-4"
+          :to="{ name: 'Product', params: { slug: product.slug } }"
+        >
+          <v-card-text class="text-center">
+            <v-img
               class="v-card--link"
               height="125"
               contain
               :src="product.image"
-          />
-          <div class="mt-4">
-            <v-btn
+            />
+            <div class="mt-4">
+              <v-btn
                 elevation="0"
                 color="secondary"
                 class="text-capitalize font-weight-bold title"
-                :to="{ name: 'Product', params: { slug: product.slug } }"
-            >{{ product.name }}
-            </v-btn>
-          </div>
-          <div class="subtitle my-2 font-weight-bold">
-          $ {{ product.price  }}
-          </div>
-          <v-btn
+                >{{ product.name }}
+              </v-btn>
+            </div>
+            <div class="subtitle my-2 font-weight-bold">
+              $ {{ product.price }}
+            </div>
+            <v-btn
               elevation="0"
               small
               rounded
               @click="addProductToCart(product)"
-          >Add to cart
-          </v-btn>
-        </v-card-text>
-      </v-card>
-    </v-col>
-     </v-row>
+              >Add to cart
+            </v-btn>
+          </v-card-text>
+        </v-card>
+      </v-col>
+    </v-row>
   </v-container>
-
-
 </template>
 
 <script>
 export default {
-  name: "ProductList",
-  methods:{
+  name: 'ProductList',
+  methods: {
     addProductToCart(product) {
-      this.$store.commit("cart/addProductToCart",product)
-      this.$store.commit("cart/toggleDrawer",true)
-    }
+      this.$store.commit('cart/addProductToCart', product);
+      this.$store.commit('cart/toggleDrawer', true);
+    },
   },
-  computed:{
-    products(){
+  computed: {
+    products() {
       return this.$store.state.product.products;
-    }
+    },
   },
-}
+};
 </script>
